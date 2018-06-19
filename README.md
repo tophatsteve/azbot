cd80bf09c8a1b1f213ded08dabc989c8e9834f88
Steve Bentley <steve@codebazaar.co.uk>
2018-06-19 08:22:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# azbot
+
+Using [Gobot](https://gobot.io) to connect to [Azure Iot Hub](https://azure.microsoft.com/en-gb/services/iot-hub/) with [MQTT](http://mqtt.org/).
+
---
+++ azbot.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/mqtt"
+)
+
+func main() {
+	deviceID := os.Getenv("MQTT_DEVICE_ID")
+	username := os.Getenv("MQTT_USERNAME")
+	password := os.Getenv("MQTT_PASSWORD")
+	host := os.Getenv("MQTT_HOST")
+	port := 8883
+	brokerURL := fmt.Sprintf("tcps://%s:%d", host, port)
+
+	mqttAdaptor := mqtt.NewAdaptorWithAuth(
+		brokerURL,
+		deviceID,
+		username,
+		password,
+	)
+
+	mqttAdaptor.SetServerCert("BaltimoreRootCertificate.cer")
+	mqttAdaptor.SetUseSSL(true)
+
+	err := mqttAdaptor.Connect()
+
+	work := func() {
+		data := []byte("hello")
+		eventTopic := fmt.Sprintf("devices/%s/messages/events/", deviceID)
+
+		gobot.Every(5*time.Second, func() {
+			fmt.Printf("Sending '%s' to IoT Hub topic %s\n", data, eventTopic)
+			mqttAdaptor.Publish(eventTopic, data)
+		})
+	}
+
+	robot := gobot.NewRobot("azbot",
+		[]gobot.Connection{mqttAdaptor},
+		work,
+	)
+
+	err = robot.Start()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
