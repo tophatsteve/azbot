8666f11ae4c4aea15145794555405c9139701881
Steve Bentley <steve@codebazaar.co.uk>
2018-06-22 12:55:54 +0100

    Handle incoming messages

--- azbot.go
+++ azbot.go
@@ -32,6 +32,11 @@ func main() {
 	work := func() {
 		data := []byte("hello")
 		eventTopic := fmt.Sprintf("devices/%s/messages/events/", deviceID)
+		incomingMessages := fmt.Sprintf("devices/%s/messages/devicebound/#", deviceID)
+
+		mqttAdaptor.On(incomingMessages, func(msg mqtt.Message) {
+			fmt.Printf("Receieved message: '%s' in topic '%s' with id '%d'\n", string(msg.Payload()[:]), msg.Topic(), msg.MessageID())
+		})
 
 		gobot.Every(5*time.Second, func() {
 			fmt.Printf("Sending '%s' to IoT Hub topic %s\n", data, eventTopic)
